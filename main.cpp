08184da04348e9a96e985680d2452687e98f2cad
R0me0Star <eip_spb@mail.ru>
2026-03-05 14:16:41 +0300

    loop. memory delete. finished implementation

--- main.cpp
+++ main.cpp
@@ -31,3 +31,55 @@ template < class T > CircularBiList< T >::CircularBiList()
   fake->prev = fake;
   list_size = 0;
 }
+
+template < class T > CircularBiList< T >::~CircularBiList()
+{
+  BiList< T > *current = fake->next;
+
+  while (current != fake) {
+    BiList< T > *to_delete = current;
+    current = current->next;
+    delete to_delete;
+  }
+
+  delete fake;
+}
+
+template < class T > void CircularBiList< T >::push_back(const T &value)
+{
+  BiList< T > *new_node = new BiList< T >();
+  new_node->val = value;
+
+  new_node->next = fake;
+  new_node->prev = fake->prev;
+
+  fake->prev->next = new_node;
+  fake->prev = new_node;
+
+  ++list_size;
+}
+
+template < class T > void CircularBiList< T >::pop_back()
+{
+  if (is_empty()) {
+    return;
+  }
+
+  BiList< T > *last_node = fake->prev;
+
+  last_node->prev->next = fake;
+  fake->prev = last_node->prev;
+
+  delete last_node;
+  --list_size;
+}
+
+template < class T > bool CircularBiList< T >::is_empty() const noexcept
+{
+  return list_size == 0;
+}
+
+template < class T > size_t CircularBiList< T >::size() const noexcept
+{
+  return list_size;
+}
